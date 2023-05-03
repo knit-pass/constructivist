3ff134210bffbbd6a7e5c5c2faaba58241d7b874
Prajwal Patil <prajwalpatilk@gmail.com>
2023-05-03 11:29:38 +0530

    Temp fix logger

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from src import *
 
 
 def main():
+    Logger.start_log()
     transformers_demo()
+    Logger.end_log()
 
 
 if __name__ == "__main__":
--- src/transformers.py
+++ src/transformers.py
@@ -1,4 +1,5 @@
 from transformers import pipeline
+from .logger import *
 
 pipe = pipeline(model="facebook/bart-large-mnli")
 
@@ -38,40 +39,50 @@ categories_refined = [
 
 
 def get_categories_cap(entity: str):
-    result = pipe(entity, candidate_labels=categories_refined)
-    result_c = result["labels"]
-    result_p = result["scores"]
-    threshold = 0.1 * result_p[0]
-    j = 0
-    score_sum = 0
-    for i in result_p:
-        if i < threshold:
-            break
-        score_sum += i
-        j += 1
-    result_p = result_p[0:j]
-    result_c = result_c[0:j]
-    balance = 1 - score_sum
     result_dictionary = {}
-    for idx, i in enumerate(result_p):
-        result_p[idx] += i * balance
-        result_dictionary[result_c[idx]] = result_p[idx]
+    try:
+        result = pipe(entity, candidate_labels=categories_refined)
+        result_c = result["labels"]
+        result_p = result["scores"]
+        threshold = 0.1 * result_p[0]
+        j = 0
+        score_sum = 0
+        for i in result_p:
+            if i < threshold:
+                break
+            score_sum += i
+            j += 1
+        result_p = result_p[0:j]
+        result_c = result_c[0:j]
+        balance = 1 - score_sum
+        for idx, i in enumerate(result_p):
+            result_p[idx] += i * balance
+            result_dictionary[result_c[idx]] = result_p[idx]
+        Logger.write_debug(f"Categories fetched for(cap): {entity}")
+        Logger.write_debug(str(result_dictionary))
+    except Exception as e:
+        Logger.write_error(str(e))
     return result_dictionary
 
 
 def get_categories(entity: str):
-    result = pipe(entity, candidate_labels=categories_refined)
-    result_c = result["labels"]
-    result_p = result["scores"]
     result_dictionary = {}
-    for idx, i in enumerate(result_p):
-        result_dictionary[result_c[idx]] = result_p[idx]
+    try:
+        result = pipe(entity, candidate_labels=categories_refined)
+        result_c = result["labels"]
+        result_p = result["scores"]
+        for idx, i in enumerate(result_p):
+            result_dictionary[result_c[idx]] = result_p[idx]
+        Logger.write_debug(f"Categories fetched for: {entity}")
+        Logger.write_debug(str(result_dictionary))
+    except Exception as e:
+        Logger.write_error(str(e))
     return result_dictionary
 
 
 def transformers_demo():
-    print(get_categories("Inflation"))
-    print(get_categories_cap("Inflation"))
+    get_categories("Inflation")
+    get_categories_cap("Inflation")
 
 
 if __name__ == "__main__":
